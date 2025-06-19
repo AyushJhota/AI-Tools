a47a558a7d50eba8b67e10b99b695d8acbe6c532
ayushjhota8 <ayushjhota8@gmail.com>
2025-06-19 12:53:00 +0000

    Update base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI-Tools/', // Set the base path for deployment
   build: {
     outDir: 'docs', // Specify the output directory as 'docs'
   },
